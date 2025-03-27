a2c612b7b505e157118bdd669ebc160eff1f801f
Samueljain26 <samueljain77@gmail.com>
2025-03-27 14:28:32 +0530

    [Samuel] ADD:creating product class.

---
+++ src/com/bridgelabz/onlinemarketplace/Product.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.onlinemarketplace;
+
+abstract class Product<T> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public abstract void displayProduct();
+}
