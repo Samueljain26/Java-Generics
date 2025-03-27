9575b21d7c13d87b50c4cbb3b4ae2bd190656421
Samueljain26 <samueljain77@gmail.com>
2025-03-27 10:46:31 +0530

    [Samuel] ADD: Making flight.java

---
+++ src/com/bridgelabz/flightScheduling/Flight.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.flightScheduling;
+
+public class Flight<T> {
+    private T flightNumber;
+    private String destination;
+    private String departureTime;
+
+    public Flight(T flightNumber, String destination, String departureTime){
+        this.flightNumber =flightNumber;
+        this.destination =destination;
+        this.departureTime =departureTime;
+    }
+    public T getFlightNumber(){
+        return flightNumber;
+    }
+    public void displayFlight(){
+        System.out.println("Flight Number: "+ flightNumber+ " -> Destination: "+ destination+ " -> Departure Time: " +departureTime);
+    }
+}
