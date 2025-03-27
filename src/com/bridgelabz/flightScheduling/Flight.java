45b520b6c20793378c9ab19c67b439609d8f7147
Samueljain26 <samueljain77@gmail.com>
2025-03-27 11:03:17 +0530

    [Samuel] ADD: Updating flight.java

--- src/com/bridgelabz/flightScheduling/Flight.java
+++ src/com/bridgelabz/flightScheduling/Flight.java
@@ -2,18 +2,18 @@ package com.bridgelabz.flightScheduling;
 
 public class Flight<T> {
     private T flightNumber;
+    private String departure;
     private String destination;
-    private String departureTime;
 
-    public Flight(T flightNumber, String destination, String departureTime){
+    public Flight(T flightNumber, String departure, String destination){
         this.flightNumber =flightNumber;
         this.destination =destination;
-        this.departureTime =departureTime;
+        this.departure =departure;
     }
     public T getFlightNumber(){
         return flightNumber;
     }
     public void displayFlight(){
-        System.out.println("Flight Number: "+ flightNumber+ " -> Destination: "+ destination+ " -> Departure Time: " + departureTime);
+        System.out.println("Flight Number: "+ flightNumber+ " -> From: "+ departure+ " -> TO: " + destination);
     }
 }
