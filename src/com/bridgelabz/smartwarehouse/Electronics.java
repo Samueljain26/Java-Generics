b677dfa12cd55def91ac3a5358b2ee83aafa279a
Samueljain26 <samueljain77@gmail.com>
2025-03-27 11:47:14 +0530

    [Samuel] ADD: Creating Electronics.java

--- src/com/bridgelabz/smartwarehouse/Electronics.java
+++ src/com/bridgelabz/smartwarehouse/Electronics.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.smartwarehouse;
 
-public class Electronics {
+class Electronics extends WarehouseItem {
+    private String brand;
+
+    public Electronics(String name, double price, String brand) {
+        super(name, price);
+        this.brand = brand;
+    }
+
+    @Override
+    public void displayItem() {
+        System.out.println("Electronics: " + getName() + " | Brand: " + brand + " | Price: " + getPrice());
+    }
 }
